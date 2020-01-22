a66f02e324eb284fad05f34ee1e0b624a84a808c
Junzhi Sheng <jsheng0901@gmail.com>
2020-01-22 11:16:34 -0500

    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,6 +2,18 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+    /** check if it is leap year*/
+    public static boolean isLeapYear(int year){
+      if (year % 4 == 0 && year % 100 != 0){
+        return true;
+      }
+      else if (year % 400 == 0){
+        return true;
+      }
+      else{ 
+        return false;
+      }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
